637a1af4ba0e1f2b011a36d3be619847500e3586
oumseyoung <seyoungoum04@gmail.com>
2026-06-24 23:19:07 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,8 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(count+1):
+        total += price * i
+    if money >= total:
+        return 0
+    else:
+        return total - money
