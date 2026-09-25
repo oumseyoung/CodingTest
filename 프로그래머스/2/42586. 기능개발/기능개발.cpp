4e64e4e08be2192b106e2cf793fa55b082c6c349
oumseyoung <seyoungoum04@gmail.com>
2026-09-26 01:48:47 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,23 +5,17 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> days(progresses.size(), 0);
-    for(int i=0; i<progresses.size(); i++) {
-        days[i] = (99 - progresses[i] + speeds[i]) / speeds[i];
-    }
-    int start = 0;
-    int i, cnt;
-    while(start < days.size()) {
-        cnt = 1;
-        for(i=start+1; i<progresses.size(); i++) {
-            if(days[start] >= days[i]) {
-                cnt++;
-            }
-            else { break; }
+    int cnt = 1;
+    int first = (100 - progresses[0] + speeds[0] -1) / speeds[0];
+
+    for(int i=1; i<progresses.size(); i++) {
+        int day = (100 - progresses[i] + speeds[i] -1) / speeds[i];
+        if(first >= day) {
+            cnt++;
         }
-        start = i;
-        answer.push_back(cnt);
+        else { answer.push_back(cnt); cnt = 1; first = day; }
     }
+    answer.push_back(cnt);
     
     return answer;
 }
